732cbe57a5044437e7b988a4d6819d12be515b71
SHANKAR JADAV <shankarjadav360@gmail.com>
2024-04-12 14:31:44 +0530

    push

---
+++ auto_scale.tf
@@ -0,0 +1,79 @@
+resource "aws_autoscaling_group" "aws_asg" {
+  availability_zones  = data.aws_availability_zones.aws_zones.names
+  desired_capacity    = 1
+  max_size            = 1
+  min_size            = 1
+  vpc_zone_identifier = [aws_subnet.public.id, aws_subnet.private.id]
+
+  health_check_type = "ELB"
+
+  load_balancers = aws_elb.web_elb.id
+
+  launch_template {
+    id      = aws_launch_template.wordpress.id
+    version = "$Latest"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "wordpress"
+    propagate_at_launch = true
+  }
+}
+
+
+# aws auto scaling poilicy
+
+resource "aws_autoscaling_policy" "web_policy_up" {
+  name = "web_policy_up"
+  scaling_adjustment = 1
+  adjustment_type = "ChangeInCapacity"
+  cooldown = 300
+  autoscaling_group_name = aws_autoscaling_group.aws_asg.name
+}
+
+resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_up" {
+  alarm_name = "web_cpu_alarm_up"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods = "2"
+  metric_name = "CPUUtilization"
+  namespace = "AWS/EC2"
+  period = "120"
+  statistic = "Average"
+  threshold = "70"
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.aws_asg.name
+  }
+  alarm_description = "This metric monitor EC2 instance CPU utilization"
+  alarm_actions = [ "${aws_autoscaling_policy.web_policy_up.arn}" ]
+}
+
+resource "aws_autoscaling_policy" "web_policy_down" {
+  name = "web_policy_down"
+  scaling_adjustment = -1
+  adjustment_type = "ChangeInCapacity"
+  cooldown = 300
+  autoscaling_group_name = aws_autoscaling_group.aws_asg.name
+}
+
+resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_down" {
+  alarm_name = "web_cpu_alarm_down"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods = "2"
+  metric_name = "CPUUtilization"
+  namespace = "AWS/EC2"
+  period = "120"
+  statistic = "Average"
+  threshold = "30"
+  
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.aws_asg.name
+  }
+  
+  alarm_description = "This metric monitor EC2 instance CPU utilization"
+  alarm_actions = [ "${aws_autoscaling_policy.web_policy_down.arn}" ]
+}
